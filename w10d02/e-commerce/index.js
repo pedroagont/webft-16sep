313a2151d9d1b379f83a77b57553662bb7b164e8
Pedro A. González <pgonzalez@post.com>
2024-11-19 13:41:58 -0500

    add express initial setup with logger and ejs

--- w10d02/e-commerce/index.js
+++ w10d02/e-commerce/index.js
@@ -1,1 +1,25 @@
-console.log('Hello from app!');
+// REQUIREMENTS
+const express = require('express');
+const morgan = require('morgan');
+
+// SETUP & MIDDLEWARE
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(morgan('dev'));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+// ROUTES
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'Hi from express!' });
+});
+
+// LISTENER
+app.listen(PORT, () => {
+  console.log('Listening at port:', PORT);
+});
